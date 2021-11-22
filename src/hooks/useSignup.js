e1f704ec72c60bc94f6cb35f3002cb59aaec9991
Daniel O'Brien <daniel@monomania.ie>
2021-11-22 17:42:49 +0000

    create user document to store some global state

--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { projectAuth, projectStorage } from '../firebase/config';
+import { projectAuth, projectStorage, projectFirestore } from '../firebase/config';
 import { useAuthContext } from './useAuthContext';
 
 export const useSignup = () => {
@@ -27,6 +27,13 @@ export const useSignup = () => {
 
       // add display name to user
       await response.user.updateProfile({ displayName, photoURL: imageUrl });
+
+      // create a user document
+      await projectFirestore.collection('users').doc(response.user.uid).set({
+        online: true,
+        displayName,
+        photoURL: imageUrl,
+      });
 
       // dispatch login action
       dispatch({ type: 'LOGIN', payload: response.user });
