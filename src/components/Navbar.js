2ca560fb27816e6a4b2e4f7458da9cf29c6099f3
Daniel O'Brien <daniel@monomania.ie>
2021-11-23 15:07:42 +0000

    Connected logout hook, dynamically present button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,15 @@
 import { Link } from 'react-router-dom';
+import { useLogout } from '../hooks/useLogout';
+import { useAuthContext } from '../hooks/useAuthContext';
 
 // styles & images
 import './Navbar.css';
 import Temple from '../assets/temple.svg';
 
 export default function Navbar() {
+  const { isPending, logout } = useLogout();
+  const { user } = useAuthContext();
+
   return (
     <div className='navbar'>
       <ul>
@@ -12,15 +17,29 @@ export default function Navbar() {
           <img src={Temple} alt='App Logo' />
           <span>Manage</span>
         </li>
-        <li>
-          <Link to='/login'>Login</Link>
-        </li>
-        <li>
-          <Link to='/signup'>Sign Up</Link>
-        </li>
-        <li>
-          <button className='btn'>Logout</button>
-        </li>
+        {!user && (
+          <>
+            <li>
+              <Link to='/login'>Login</Link>
+            </li>
+            <li>
+              <Link to='/signup'>Sign Up</Link>
+            </li>
+          </>
+        )}
+        {user && (
+          <li>
+            {!isPending ? (
+              <button className='btn' onClick={logout}>
+                Logout
+              </button>
+            ) : (
+              <button className='btn' disabled>
+                Logging out..
+              </button>
+            )}
+          </li>
+        )}
       </ul>
     </div>
   );
