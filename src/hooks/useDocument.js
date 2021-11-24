bb053da5ba025889cb9f70b1cc07a71b565eefab
Daniel O'Brien <daniel@monomania.ie>
2021-11-24 16:22:58 +0000

    basic realtime data for document hook

---
+++ src/hooks/useDocument.js
@@ -0,0 +1,32 @@
+import { useEffect, useState } from 'react';
+import { projectFirestore } from '../firebase/config';
+
+export const useDocument = (collection, id) => {
+  const [document, setDocument] = useState(null);
+  const [error, setError] = useState(null);
+
+  // realtime data for the document
+
+  useEffect(() => {
+    const ref = projectFirestore.collection(collection).doc(id);
+
+    const unsubscribe = ref.onSnapshot(
+      (snapshot) => {
+        setDocument({ ...snapshot.data(), id: snapshot.id });
+        setError(null);
+      },
+      (error) => {
+        console.log(error.message);
+        setError('Failed to get document');
+      }
+    );
+    return () => {
+      unsubscribe();
+    };
+  }, [collection, id]);
+
+  return {
+    document,
+    error,
+  };
+};
