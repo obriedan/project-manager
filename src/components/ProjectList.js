8fab8e8edf8700c7ec1d69ab9495faf22cb4b504
Daniel O'Brien <daniel@monomania.ie>
2021-11-24 15:46:32 +0000

    init project list + styling

---
+++ src/components/ProjectList.js
@@ -0,0 +1,31 @@
+//hooks
+import { Link } from 'react-router-dom';
+
+//components
+import Avatar from './Avatar';
+
+// styles
+import './ProjectList.css';
+
+export default function ProjectList({ projects }) {
+  return (
+    <div className='project-list'>
+      {!projects.length && <p>No projects</p>}
+      {projects.map((project) => (
+        <Link to={`/projects/${project.id}`} key={project.id}>
+          <h4>{project.name}</h4>
+          <p> Due by {project.dueDate.toDate().toDateString()}</p>
+          <div className='assigned-to'>
+            <ul>
+              {project.assignedUsersList.map((user) => (
+                <li key={user.photoURL}>
+                  <Avatar src={user.photoURL} />
+                </li>
+              ))}
+            </ul>
+          </div>
+        </Link>
+      ))}
+    </div>
+  );
+}
