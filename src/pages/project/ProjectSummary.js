d8eb67e01b35cca5a0777aec9cc224471220476e
Daniel O'Brien <daniel@monomania.ie>
2021-11-25 15:05:58 +0000

    Added delete document button + method

--- src/pages/project/ProjectSummary.js
+++ src/pages/project/ProjectSummary.js
@@ -1,10 +1,26 @@
+// hooks
+import { useFirestore } from '../../hooks/useFirestore';
+import { useAuthContext } from '../../hooks/useAuthContext';
+import { useNavigate } from 'react-router-dom';
+
+// components
 import Avatar from '../../components/Avatar';
 
 export default function ProjectSummary({ project }) {
+  const { deleteDocument } = useFirestore('projects');
+  const { user } = useAuthContext();
+  const navigate = useNavigate();
+
+  const handleClick = (e) => {
+    deleteDocument(project.id);
+    navigate('/');
+  };
+
   return (
     <div>
       <div className='project-summary'>
         <h2 className='page-title'>{project.name}</h2>
+        <p>By {project.createdBy.displayName}</p>
         <p className='due-date'>Project due by {project.dueDate.toDate().toDateString()}</p>
         <p className='details'>{project.details}</p>
         <h4>Project is assigned to:</h4>
@@ -16,6 +32,11 @@ export default function ProjectSummary({ project }) {
           ))}
         </div>
       </div>
+      {user.uid === project.createdBy.id && (
+        <button onClick={handleClick} className='btn'>
+          Mark as Complete
+        </button>
+      )}
     </div>
   );
 }
