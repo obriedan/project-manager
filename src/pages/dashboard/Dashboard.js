ed3182cf3d6a1406174ea21e59e2705097c3f414
Daniel O'Brien <daniel@monomania.ie>
2021-11-25 19:16:03 +0000

    added filter method -- still needs updating

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,18 +1,49 @@
 //hooks
 import { useCollection } from '../../hooks/useCollection';
+import { useState } from 'react';
+import { useAuthContext } from '../../hooks/useAuthContext';
 
 //components
 import ProjectList from '../../components/ProjectList';
+import ProjectFilter from './ProjectFilter';
 
 // styles
 import './Dashboard.css';
+
 export default function Dashboard() {
   const { documents, error } = useCollection('projects');
+  const [currentFilter, setCurrentFilter] = useState('all');
+  const { user } = useAuthContext();
+
+  const projects = documents
+    ? documents.filter((document) => {
+        switch (currentFilter) {
+          case 'all':
+            return true;
+          case 'mine':
+            let assignedToMe = false;
+            document.assignedUsersList.forEach((u) => {
+              if (user.uid === u.id) {
+                assignedToMe = true;
+              }
+            });
+            return assignedToMe;
+          default:
+            return document.category === currentFilter;
+        }
+      })
+    : null;
+
+  const changeFilter = (newFilter) => {
+    setCurrentFilter(newFilter);
+  };
+
   return (
     <div>
       <h2 className='page-title'>Dashboard</h2>
+      {documents && <ProjectFilter currentFilter={currentFilter} changeFilter={changeFilter} />}
       {error && <p className='error'>{error}</p>}
-      {documents && <ProjectList projects={documents} />}
+      {projects && <ProjectList projects={projects} />}
     </div>
   );
 }
